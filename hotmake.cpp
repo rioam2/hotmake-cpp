9c737e39c632f9700de09702be4fb3e8f6ca0ebd
rioam2 <rioam2@illinois.edu>
2019-01-08 12:37:18 -0600

    implemented async progress spinner for build

--- hotmake.cpp
+++ hotmake.cpp
@@ -1,20 +1,25 @@
 #include <stdlib.h>
+#include <unistd.h>
 #include <array>
 #include <cstdio>
 #include <exception>
+#include <future>
 #include <iostream>
 #include <memory>
 #include <stdexcept>
 #include <string>
+#include <vector>
 #include "MakeRecord.h"
 #include "inotify-cxx/inotify-cxx.h"
 
+using std::async;
 using std::cerr;
 using std::cout;
 using std::endl;
 using std::exception;
 using std::string;
 using std::system;
+using std::vector;
 
 /**
  * Prints the CLI usage to the terminal and returns
@@ -26,6 +31,22 @@ int printUsage(char* executable) {
 }
 
 /**
+ * Helps print interactive lines to the terminal which overwrite each other.
+ * Flushes cout and uses carrage returns.
+ */
+void printInteractive(string str) {
+    /* Keep track of last string printed to display so can be overwritten */
+    static string lastPrint = str;
+    cout << "\r";
+    /* Print over last interactive line */
+    for (size_t i = 0; i < lastPrint.size(); i++) cout << " ";
+    /* Print new line over existing one */
+    cout << "\r" << str;
+    std::flush(cout);
+    lastPrint = str;
+}
+
+/**
  * POSIX execution wrapper to enable stdout capture.
  * Originally Posted on StackOverflow: 
  * https://stackoverflow.com/questions/478898/how-do-i-execute-a-command-and-get-output-of-command-within-c-using-posix
@@ -34,10 +55,8 @@ std::string exec(const char* cmd) {
     std::array<char, 128> buffer;
     std::string result;
     std::unique_ptr<FILE, decltype(&pclose)> pipe(popen(cmd, "r"), pclose);
-    if (!pipe) {
-        throw std::runtime_error("popen() failed!");
-    }
-    while (fgets(buffer.data(), buffer.size(), pipe.get()) != nullptr) {
+    if (!pipe) throw std::runtime_error("execution of target failed.");
+    while (fgets(buffer.data(), buffer.size(), pipe.get()) != NULL) {
         result += buffer.data();
     }
     return result;
@@ -47,25 +66,50 @@ int main(int argc, char* argv[]) {
     /* Primitive exception handling */
     try {
         MakeRecord makefile;
-        auto [target, deps] =
-            argc >= 2 ? makefile.getRule(argv[1]) : makefile.getRule();
+        auto [target, deps] = (argc >= 2) ? makefile.getRule(argv[1]) : makefile.getRule();
         /* Assemble executable command */
         const char* executable = string("./" + target).c_str();
         /* Watch all dependent files */
         Inotify notify;
-        cout << "Watching source files: " << endl;
+        system("clear"); /* Clear terminal window */
+        cout << "\033[1;37mHotmake v1.0:\033[0m Target: " << target << "\n"
+             << endl;
+        cout << "\033[1;37mWatching source files for changes:\033[0m\n";
         for (string dep : deps) {
             dep = "./" + dep;
-            cout << "\033[1;32m + " << dep << "\033[0m\n";
+            cout << " + " << dep << endl;
             InotifyWatch watch(dep, IN_MODIFY);
             notify.Add(watch);
         }
-        while (1) {
-            /* Squash redundant fs triggers */
-            notify.WaitForEvents() && notify.WaitForEvents();
-            if (0 != notify.GetEventCount()) {
-                cout << exec("make");
-            }
+        cout << endl;
+        auto interactiveBuild = [&]() {
+            bool buildFailed = false;
+            bool buildComplete = false;
+            auto build = async(std::launch::async, [&] {
+                string result = exec("make");
+                buildComplete = true;
+            });
+            auto printProgress = async(std::launch::async, [&] {
+                string color_building = "\033[1;93m";
+                string color_complete = "\033[1;32m";
+                string base = "Building changes";
+                printInteractive(color_building + "  " + base);
+                /* Spinner animation courtesy of cli-spinners on GitHub :) - thank you! */
+                vector<string> spinner = {"⠋", "⠙", "⠹", "⠸", "⠼", "⠴", "⠦", "⠧", "⠇", "⠏"};
+                unsigned frame = 0;
+                while (!buildComplete) {
+                    cout << "\r" << color_building << spinner[frame] << " " << base;
+                    std::flush(cout);
+                    frame = (++frame) % spinner.size();
+                    usleep(70000);
+                }
+                printInteractive(color_complete + "✔ Build complete");
+            });
+        };
+        interactiveBuild(); /* Run build on startup */
+        /* Watch for file updates and squash redundant triggers */
+        while (notify.WaitForEvents() && notify.WaitForEvents()) {
+            if (0 != notify.GetEventCount()) interactiveBuild();
         }
     } catch (const char* e) {
         cerr << e << endl;
